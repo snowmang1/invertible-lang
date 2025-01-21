c0496044c945ec8aaca172ff05d6d0b08302a975
Evan Drake <evanmdrake@icloud.com>
2025-01-20 18:10:03 -0800

    created inversion file and function for rearranging BFSTree for inversion
    Signed-off-by: Evan Drake <evanmdrake@icloud.com>

---
+++ src/ReArrange.hs
@@ -0,0 +1,9 @@
+module ReArrange ( module ReArrange ) where
+
+import Data
+import BFS
+
+-- | This creates the inverse function of f, f^{-1}
+-- limited to arithmetic scope, well formed BFSTree and invertable functions
+invertBFSTree :: BFSTree -> BFSTree
+invertBFSTree tree = undefined
