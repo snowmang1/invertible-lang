22c0ffc071de087f578cdb97a48f259a3e05b868
Evan Drake <evanmdrake@icloud.com>
2025-01-30 12:44:59 -0800

    arithmetic inversion of arbitrary size tree works
    Signed-off-by: Evan Drake <evanmdrake@icloud.com>

--- src/ReArrange.hs
+++ src/ReArrange.hs
@@ -6,4 +6,12 @@ import BFS
 -- | This creates the inverse function of f, f^{-1}
 -- limited to arithmetic scope, well formed BFSTree and invertable functions
 invertBFSTree :: BFSTree -> BFSTree
-invertBFSTree tree = undefined
+invertBFSTree BFS_Nil             = BFS_Nil
+invertBFSTree (BFS_Leaf node)     = BFS_Leaf (typeInversion node)
+invertBFSTree (BFS_Delta node ls) = BFS_Delta (typeInversion node) (map invertBFSTree ls)
+
+-- | function to invert any defined OpNode
+typeInversion :: BFSTreeNode -> BFSTreeNode
+typeInversion (Add, c, d, p) = (Sub, c, d, p)
+typeInversion (Sub, c, d, p) = (Add, c, d, p)
+typeInversion op  = undefined
